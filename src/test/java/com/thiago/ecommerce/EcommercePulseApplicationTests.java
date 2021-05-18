644d738ea9d1bfab7e110c34bbb5182592297400
Thiago Farias <thiago.farias@acad.ifma.edu.br>
2021-05-18 12:26:23 -0300

    adicionado as imagens da api e do banco para o docker e configurado os arquivos dockerfile e yml

---
+++ src/test/java/com/thiago/ecommerce/EcommercePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
